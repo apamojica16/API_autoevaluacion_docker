bc1c6ae59bf4c1c96e0ccb79b33264ca3b277198
Alejandro Parada <35151015+apamojica16@users.noreply.github.com>
2018-08-27 00:34:35 -0500

    apamojica16

---
+++ API_autoevaluacion_docker/Controllers/NotasController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API_autoevaluacion_docker.Models;
+
+namespace API_autoevaluacion_docker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotasController : ControllerBase
+    {
+
+        Autoevaluaciones[] notas = new Autoevaluaciones[]{
+ new Autoevaluaciones{name="Lab1", nota=4.5},
+ new Autoevaluaciones {name="Lab2", nota=4.0},
+ new Autoevaluaciones {name="Lab3", nota=4.0},
+ new Autoevaluaciones {name="Blog", nota=4.5}
+        };
+
+        [HttpGet]
+         public IEnumerable<Autoevaluaciones> GetAllProducts()
+         {
+             return notas;
+         }
+
+
+
+        /* public Autoevaluaciones GetProduct(string id)
+         {
+             var snota = (notas.FirstOrDefault((p) => p.name == id));
+             if (snota == null)
+             {
+                 throw new HttpResponseException(
+                     Request.CreateResponse(HttpStatusCode.NotFound));
+             }
+             return snota;
+         }*/
+
+        /* public IHttpActionResult GetProduct(int id)
+         {
+             var product = notas.FirstOrDefault((p) => p.Id == id);
+             if (product == null)
+             {
+                 return NotFound();
+             }
+             return Ok(product);
+         }*/
+        [HttpGet("{id}")]
+          public ActionResult<Autoevaluaciones> GetById(string id)
+          {
+              var item = notas.FirstOrDefault(p => p.name == id);
+              if (item == null)
+              {
+                  return NotFound();
+              }
+              return item;
+          }
+
+
+    }
+}
